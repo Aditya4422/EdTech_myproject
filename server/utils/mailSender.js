9ce0af3040b55c2a0ca77672174d7477adb64c62
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-26 21:05:04 +0530

    parametes updated

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -10,7 +10,8 @@ const mailSender = async (email, title, body) => {
             auth:{
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            }
+            },
+            secure: false,
         });
 
         let info = await transporter.sendMail({
