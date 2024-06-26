58d8cebd14f5ad6e5a7b5544a2b8ba2886ab27f6
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-26 20:57:30 +0530

    instructor Dashboard route has been added

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {auth} = require('../middlewares/auth');
+const {auth, isInstructor} = require('../middlewares/auth');
 
-const {deleteProfile, updateDisplayPicture, updateProfile, getAllUserDetails, getEnrolledCourses} = require('../controllers/Profile');
+const {deleteProfile, updateProfile, updateDisplayPicture, getAllUserDetails, getEnrolledCourses, instructorDashboard} = require('../controllers/Profile');
 
 // Profile Routes
-router.delete('/deleteProfile', deleteProfile);
+router.delete('/deleteProfile',auth, deleteProfile);
 router.put('/updateProfile', auth, updateProfile);
 router.get('/getUserDetails', auth, getAllUserDetails);
 router.get('/getEnrolledCourses', auth, getEnrolledCourses);
 router.put('/updateDisplayPicture', auth, updateDisplayPicture);
+router.get('/instructorDashboard', auth, isInstructor, instructorDashboard);
 
 module.exports = router;
