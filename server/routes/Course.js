84e1cb61243b3df0b46da869b103df46db967b59
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-26 20:39:28 +0530

    Added the getInstructorCourses route

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -12,30 +12,33 @@ const {createSubSection, updateSubSection, deleteSubSection} = require('../contr
 
 const {createRating, getAllRating, getAverageRating} = require('../controllers/RatingAndReview');
 
+const { updateCourseProgress } = require('../controllers/courseProgress');
+
 const {auth, isAdmin, isInstructor, isStudent} = require('../middlewares/auth');
 // const { create } = require('../models/Category');
 
 // -----------------------------------ROUTES-----------------------------------------------------------------------------------
 // 1. Course Routes
 router.post('/createCourse', auth, isInstructor, createCourse);  // only created by course instructor
+router.post('/editCourse', auth, isInstructor, editCourse);
 router.post('/addSection', auth, isInstructor, createSection); // add section to a course
 router.post('/updateSection', auth, isInstructor, updateSection); // update a section
 router.post('/deleteSection', auth, isInstructor, deleteSection); // delete a section
 router.post('/addSubSection', auth, isInstructor, createSubSection); // add subsection
 router.post('/updateSubSection', auth, isInstructor, updateSubSection); // update subsection
 router.post('/deleteSubSection', auth, isInstructor, deleteSubSection); // delete subsection
+router.get("/getInstructorCourses", auth, isInstructor, getInstructorCourses);
 router.get('/getAllCourses', getAllCourses);
 router.post('/getCourseDetails', getCourseDetails);
 router.post('/getFullCourseDetails', auth, getFullCourseDetails);
-router.post('/editCourse', auth, isInstructor, editCourse);
-router.get('/getInstructorCourses', auth, isInstructor, getInstructorCourses);
+router.post("/updateCourseProgress", auth, isStudent, updateCourseProgress);
 router.delete('/deleteCourse', deleteCourse);
 
 
 // 2. Category routes (for admins)
 router.post('/createCategory', auth, isAdmin, createCategory);
 router.get('/showAllCategories', showAllCategories);
-router.get('/categoryPageDetails', categoryPageDetails);
+router.get('/getCategoryPageDetails', categoryPageDetails);
 
 // 3. Rating and Reviews
 router.post('/createRating', auth, isStudent, createRating);
