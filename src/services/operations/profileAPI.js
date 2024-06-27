ebdd92793cfcf408d22fe55ede693efef8236e18
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-27 19:17:01 +0530

    InstructorData api has been added

--- src/services/operations/profileAPI.js
+++ src/services/operations/profileAPI.js
@@ -4,7 +4,7 @@ import {apiConnector} from '../apiconnector';
 import {profileEndpoints} from '../apis';
 import {logout} from './authAPI';
 
-const {GET_USER_DETAILS_API, GET_USER_ENROLLED_COURSES_API} = profileEndpoints;
+const {GET_USER_DETAILS_API, GET_USER_ENROLLED_COURSES_API, GET_INSTRUCTOR_DATA_API} = profileEndpoints;
 
 export function getUserDetails (token, navigate) {
     return async (dispatch) => {
@@ -46,15 +46,37 @@ export async function getUserEnrolledCourses(token) {
         });
 
         if (!response.data.success) {
-            throw new Error(response.data.message)
+            throw new Error(response.data.message);
           }
-        result = response.data.data
+        result = response.data.data;
     }
     catch(error){
-        console.log("GET_USER_ENROLLED_COURSES_API API ERROR............", error)
-        toast.error("Could Not Get Enrolled Courses")
+        console.log("GET_USER_ENROLLED_COURSES_API API ERROR............", error);
+        toast.error("Could Not Get Enrolled Courses");
     }
-    toast.dismiss(toastId)
-    return result
+    toast.dismiss(toastId);
+    return result;
+}
+
+export async function getInstructorData(token) {
+    const toastId = toast.loading("Loading");
+    let result = [];
+
+    try{
+        const response = await apiConnector("GET", GET_INSTRUCTOR_DATA_API, null, {
+            Authorization: `Bearer ${token}`,
+        });
+
+        console.log("InstructorData api response : ", response);
+
+        result = response?.data?.courses;
+    }
+    catch(error){
+        console.log(error);
+        toast.error("Couldnot get insturctor courses");
+    }
+
+    toast.dismiss(toastId);
+    return result;
 }
 
