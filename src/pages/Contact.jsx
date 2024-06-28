68e890652582bc8149f5075ec70cdfdeac03fac0
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-28 15:55:11 +0530

    review slider has been added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ContactDetails from '../components/ContactPage/ContactDetails'
 import ContactForm from '../components/ContactPage/ContactForm'
 import Footer from '../components/common/Footer'
+import ReviewSlider from '../components/common/ReviewSlider'
 
 
 const Contact = () => {
@@ -20,10 +21,10 @@ const Contact = () => {
         
         <div className='relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white'>
             <h1 className='text-center text-4xl font-semibold mt-8'>Review from other learners</h1>
+            <ReviewSlider/>
         </div>
 
         <Footer/>
-
     </div>
   )
 }
