23699fcf53512dba53c0726ea8ba6b48f04f8f56
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-28 10:38:21 +0530

    Review Slider has been added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,6 +9,7 @@ import StatsComponent from '../components/core/AboutPage/Stats'
 import LearningGrid from '../components/core/AboutPage/LearningGrid'
 import ContactFormSection from '../components/core/AboutPage/ContactFormSection'
 import Footer from '../components/common/Footer'
+import ReviewSlider from '../components/common/ReviewSlider';
 
 const About = () => {
   return (
@@ -108,6 +109,7 @@ const About = () => {
             <h1 className='text-center text-4xl font-semibold mt-8'>
                 Reviews from other learners
             </h1>
+            <ReviewSlider/>
         </div>
 
         <Footer/>
