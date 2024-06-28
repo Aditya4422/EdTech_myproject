6fd6b624cb8ef4d3e5c946d3b087bf70a23fa444
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-06-28 18:31:11 +0530

    Error page has been updated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <div className=' flex justify-center text-3xl text-red'>
-        Error - 404 Not Found
+        Error - 404 Page Not Found
     </div>
   )
 }
