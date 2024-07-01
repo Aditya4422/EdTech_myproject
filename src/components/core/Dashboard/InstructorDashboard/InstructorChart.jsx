b00420458e956a376ff2c6bddb83e45dd842189a
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-07-01 09:24:34 +0530

    Chart for an instructor to visualize students and income has been added

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Chart, registerables } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-const InstructorChart = () => {
+Chart.register(...registerables);
+
+const InstructorChart = ({courses}) => {
+
+    const [currChart, setCurrChart] = useState("students");
+    
+    const generateRandomColors = (numColors) => {
+        const colors = [];
+        for(let i=0; i<numColors; i++){
+            const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+            colors.push(color);
+        }
+
+        return colors;
+    }
+
+    const chartDataStudents = {
+        labels: courses.map((course) => course.courseName),
+        datasets: [
+            {
+                data: courses.map((course) => course.totalStudentsEnrolled),
+                backgroundColor: generateRandomColors(courses.length),
+            }
+        ]
+    }
+
+    const chartIncomeData = {
+        labels: courses.map((course) => course.courseName),
+        datasets: [
+            {
+                data: courses.map((course) => course.totalAmountGenerated),
+                backgroundColor: generateRandomColors(courses.length),
+            }
+        ]
+    }
+
+    const options = { maintainAspectRatio: false };
+
   return (
-    <div>InstructorChart</div>
+    <div>
+        <p className="text-lg font-bold text-richblack-5">Visualize</p>
+
+        {/* these buttons will help to switch between income and student chart  */}
+        <div className=' space-x-4 font-semibold'>
+            <button onClick={() => setCurrChart("students")} 
+                    className={`${currChart === "students"? "bg-richblack-700 text-yellow-50" : "text-yellow-400" } rounded-sm p-1 px-3 transition-all duration-200`}
+            >
+                Students
+            </button>
+
+            <button onClick={() => setCurrChart("income")}
+                className={` ${currChart === "income" ? "bg-richblack-700 text-yellow-50" : "text-yellow-400"} rounded-sm p-1 px-3 transition-all duration-200`}
+            >
+                Income
+            </button>
+        </div>
+
+        <div className="relative mx-auto aspect-square h-full w-full">
+            <Pie data={currChart === "students" ? chartDataStudents : chartIncomeData}
+                options={options}
+            />
+        </div>
+    </div>
   )
 }
 
